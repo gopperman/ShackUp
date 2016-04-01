760c1ee1b6bbf5f0b20b6547d927912bb1aafca7
Greg Opperman <gopperman@gmail.com>
2016-04-01 14:28:18 -0400

    Gallery

--- scripts/app.js
+++ scripts/app.js
@@ -21,11 +21,12 @@ $(document).ready( function() {
 	});
 	//Gallery
 	$('.listing__gallery').unslider({
-		speed: 500,               //  The speed to animate each slide (in milliseconds)
-		delay: 0,              //  The delay between slide animations (in milliseconds)
-		complete: function() {},  //  A function that gets called after every slide animation
-		keys: true,               //  Enable keyboard (left, right) arrow shortcuts
-		dots: true,               //  Display dot navigation
-		fluid: false              //  Support responsive design. May break non-responsive designs
-	});
+		arrows: false,
+		autoplay: false,
+		speed: 500,
+		complete: function() {},
+		keys: true,               
+		nav: true,               
+		fluid: true
+	});          
 });
